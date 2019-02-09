66284f57495cf351093f9c556b4b6e2d9ff77728
richardkyu <richard.k.yu@vanderbilt.edu>
2019-02-08 23:56:38 -0500

    hw corrections

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -27,6 +27,7 @@ sortedVotes = Counter(listCandidates).most_common(voteC)
 
 #store winner
 resultsC = []
+resultsB = []
 
 #print desired output
 print(f'''Election Results
@@ -34,11 +35,31 @@ print(f'''Election Results
 Total Votes: {voteC}
 -------------------------''')
 winner = Counter(listCandidates).most_common(1)
+
 for value, count in sortedVotes:
         resultsC.append(value)
+        resultsB.append(count)
         raw = float(count/voteC)
         percent = round(raw*100,3)
         print(f"{value}: {percent}% ({int(raw*voteC)})")
+        
 
 print(f'''-------------------------
 Winner: {resultsC[0]}''')
+
+
+with open("election_results.txt", "a") as out:
+    out.write(f'''  Election Results
+    -------------------------
+    Total Votes: {voteC}
+    -------------------------
+    
+    {resultsC[0]}: {round((resultsB[0]/voteC*100),3)}% ({resultsB[0]})
+    {resultsC[1]}: {round((resultsB[1]/voteC*100),3)}% ({resultsB[1]})
+    {resultsC[2]}: {round((resultsB[2]/voteC*100),3)}% ({resultsB[2]})
+    {resultsC[3]}: {round((resultsB[3]/voteC*100),3)}% ({resultsB[3]})
+
+    -------------------------
+    Winner: {resultsC[0]}''')
+out.close()
+
